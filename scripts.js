e1e7cc7cfc404b74fdc8e61106670ee26ff01239
aslamafeerdeen <aslamafeerdeen@gmail.com>
2025-03-08 19:29:47 +0530

    new virsion

--- scripts.js
+++ scripts.js
@@ -1,4 +1,12 @@
-// Example: Log a message when the page loads
-window.onload = function () {
-    console.log("Website Loaded!");
-};
+document.querySelectorAll('.photo img').forEach(image => {
+    image.addEventListener('click', function() {
+        const modal = document.createElement('div');
+        modal.classList.add('modal');
+        modal.innerHTML = `<img src="${this.src}" alt="Large view">`;
+        document.body.appendChild(modal);
+
+        modal.addEventListener('click', () => {
+            modal.remove();
+        });
+    });
+});
